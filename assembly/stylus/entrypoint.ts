cd5d5c5809a2c15c8137f5a2a2a07232bf35cac7
OFUZORCHUKWUEMEKE <uniben2018@gmail.com>
2025-08-07 11:34:46 +0100

    changes function to average

--- assembly/stylus/entrypoint.ts
+++ assembly/stylus/entrypoint.ts
@@ -1,6 +1,7 @@
 import { getInput, sendOutput } from './stylus';
 export { mark_used } from './stylus'; // Required by Stylus
 import { main } from '../app';
+import { bytesToI32, i32ToBytes } from '../utils';
 
 // Stylus entrypoint
 export function user_entrypoint(len: i32): i32 {
@@ -9,9 +10,17 @@ export function user_entrypoint(len: i32): i32 {
     return 1;
   }
 
-  // Calling the app
-  const output = main(input);
+  // Convert Uint8Array to i32 array for processing
+  const inputArray: i32[] = [];
+  for (let i = 0; i < input.length; i += 4) {
+    const slice = input.slice(i, i + 4);
+    inputArray.push(bytesToI32(slice));
+  }
 
-  sendOutput(output);
+  // Calling the app
+  const output = main(inputArray);
+
+  // Convert i32 result back to Uint8Array
+  sendOutput(i32ToBytes(output));
   return 0;
 }
